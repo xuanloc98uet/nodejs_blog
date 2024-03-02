596acbc73cfd1070d6d5524861699df7be995559
xuanloc98uet <xuanloc98uet@gmail.com>
2024-03-02 17:05:37 +0700

    update

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './resources','views'));
+app.set('views', path.join(__dirname, './resources', 'views'));
 
 route(app);
 
